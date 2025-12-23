b9cea2de854d8887410af37070e589a94d0d73f8
gravict <leonardoedb17@gmail.com>
2025-12-23 18:30:36 +0700

    Update manageNotes.js

--- handler/admin/manageNotes.js
+++ handler/admin/manageNotes.js
@@ -2,17 +2,17 @@ import db from "../../config/database.js";
 
 export const manageNotes = async (req, res) => {
   try {
-    const { timId } = req.query;
+    const { timId } = req.query; // ini sebenarnya user_id
     const { notes } = req.body;
 
     await db.execute(
-      "UPDATE tim SET notes = ? WHERE id = ?",
-      [notes, timId]
+      "UPDATE tim SET notes = ? WHERE user_id = ?",
+      [notes ?? "", timId]
     );
 
     return res.status(200).json({
       success: true,
-      message: `Catatan tim ${timId} berhasil diperbarui`,
+      message: `Catatan tim dengan user_id ${timId} berhasil diperbarui`,
     });
   } catch (error) {
     console.error("ERROR MANAGE NOTES:", error);
